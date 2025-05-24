bffa7030fa5a1bc3d279ba1854ec89967405dd3f
sasa71928 <osalazarsalas@gmail.com>
2025-05-23 23:57:59 -0700

    Agregando Archivos (3)

---
+++ Consultorio/src/config/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+//Ejemplo Profe Brenda DB
+
+return [
+    'base_url' => 'http://localhost:81/programacion-web/programacion-web-2024/23_php_bd/public',
+    'assets_url' => 'http://localhost:81/programacion-web/programacion-web-2024/23_php_bd/public/assets',
+    'db' => [
+        'host' => '127.0.0.1',
+        'name' => 'programacion_web',
+        'user' => 'root',
+        'password' => ''
+    ]
+];
